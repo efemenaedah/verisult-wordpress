611ef605b11c943ea2aaaf889fd65abb83e12d4d
Edah Evans <efemenaedah@yahoo.com>
2026-09-16 11:43:16 -0230

    Add wp-content/themes/verisult-astra-child/assets/js/main.js

---
+++ wp-content/themes/verisult-astra-child/assets/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Verisult frontend enhancements.
+ * Keep this file dependency-free unless a documented requirement is added.
+ */
+(function () {
+  'use strict';
+
+  document.documentElement.classList.add('js');
+})();
+
